d397c5c9a3a9122b17816641f8cd85734ff3f0dd
Kamesh <kamesh.kumar.s@gmail.com>
2015-09-17 18:28:56 +1000

    initial commit

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('meanApp')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
